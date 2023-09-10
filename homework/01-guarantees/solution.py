c1df3421719a8eeba93de9256deb7425ab91996d
Алексей Пеньков <vvauijij@MacBook-Air-Aleksej.local>
2023-09-10 14:55:38 +0300

    AT MOST ONCE guarantee

--- homework/01-guarantees/solution.py
+++ homework/01-guarantees/solution.py
@@ -3,14 +3,19 @@ from dslabmp import Context, Message, Process
 
 # AT MOST ONCE ---------------------------------------------------------------------------------------------------------
 
+MESSAGE_ORDER_LIMIT = 3
+
 class AtMostOnceSender(Process):
     def __init__(self, proc_id: str, receiver_id: str):
         self._id = proc_id
         self._receiver = receiver_id
+        self._order = 0
 
     def on_local_message(self, msg: Message, ctx: Context):
         # receive message for delivery from local user
-        pass
+        msg['order'] = self._order
+        ctx.send(msg, self._receiver)
+        self._order += 1
 
     def on_message(self, msg: Message, sender: str, ctx: Context):
         # process messages from receiver here
@@ -24,6 +29,8 @@ class AtMostOnceSender(Process):
 class AtMostOnceReceiver(Process):
     def __init__(self, proc_id: str):
         self._id = proc_id
+        self._order_lower_bound = 0
+        self._order_store = set()
 
     def on_local_message(self, msg: Message, ctx: Context):
         # not used in this task
@@ -32,7 +39,18 @@ class AtMostOnceReceiver(Process):
     def on_message(self, msg: Message, sender: str, ctx: Context):
         # process messages from receiver
         # deliver message to local user with ctx.send_local()
-        pass
+        order = msg['order']
+
+        if order >= self._order_lower_bound and order not in self._order_store:
+            self._order_store.add(order)
+            msg.remove('order')
+            ctx.send_local(msg)
+
+        if all(cur_order in self._order_store for cur_order in range(order - MESSAGE_ORDER_LIMIT, order)):
+            for order_to_remove in range(self._order_lower_bound, order + 1):
+                self._order_store.discard(order_to_remove)
+            self._order_lower_bound = order + 1
+    
 
     def on_timer(self, timer_name: str, ctx: Context):
         # process fired timers here
