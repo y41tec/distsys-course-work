c81fe3ff3b5169928703657f8656fe1b1c2fc662
Alexey Penkov <vvauijij@gmail.com>
2023-10-14 12:20:51 +0300

    05-practice-messaging: 10/10

--- homework/05-practice-messaging/server/server.py
+++ homework/05-practice-messaging/server/server.py
@@ -9,70 +9,98 @@ from loguru import logger
 from typing import List, Optional
 
 
+RABBITMQ_HOST = os.getenv("RABBITMQ_HOST")
+RABBITMQ_PORT = os.getenv("RABBITMQ_PORT")
+
+WEB_HOST = os.getenv("WEB_HOST")
+WEB_PORT = os.getenv("WEB_PORT")
+IMAGES_ENDPOINT = os.getenv("IMAGES_ENDPOINT", "/api/v1.0/images")
+DATA_DIR = os.getenv("DATA_DIR")
+
+
 class Server:
     def __init__(self, host, port):
         self._counter = 0
-        self._processed_images = []
-        self.init_task_queue(host, port)
-        self.init_callback_queue(host, port)
+        self._lock = threading.Lock()
 
-    def init_task_queue(self, host, port):
+        self._pending_images = {}
+        self._processed_images = []
+
+        self._host = host
+        self._port = port
+
+        self.init_callback_queue()
+        self.init_task_queue()
+
+    def init_task_queue(self):
+        logger.info("Initializing task queue.")
         connection = pika.BlockingConnection(
-            pika.ConnectionParameters(host=host, port=port)
+            pika.ConnectionParameters(host=self._host, port=self._port)
         )
         self._task_channel = connection.channel()
-        self._task_channel.queue_declare(queue="task_queue", durable=True)
-        self._task_channel.confirm_delivery()
+        self._task_channel.queue_declare(queue="task_queue")
+        publisher_thread = threading.Thread(target=self.start_publishing)
+        publisher_thread.start()
 
-    def init_callback_queue(self, host, port):
+    def start_publishing(self):
+        while True:
+            time.sleep(0.2)
+            try:
+                with self._lock:
+                    image_id, image = list(self._pending_images.items())[0]
+                    self._task_channel.basic_publish(
+                        exchange="",
+                        routing_key="task_queue",
+                        properties=pika.BasicProperties(
+                            reply_to=self._callback_queue,
+                        ),
+                        body=f"{image} {image_id}",
+                    )
+                    logger.info(f"Server published {image_id}.")
+            except Exception as e:
+                logger.info(f"Server failed to publish due to {e}.")
+
+    def init_callback_queue(self):
+        logger.info("Initializing callback queue.")
         connection = pika.BlockingConnection(
-            pika.ConnectionParameters(host=host, port=port)
+            pika.ConnectionParameters(host=self._host, port=self._port)
         )
         channel = connection.channel()
-        callback_queue = channel.queue_declare(
-            queue="callback_queue", durable=True, exclusive=True
-        )
+        callback_queue = channel.queue_declare(queue="callback_queue", exclusive=True)
         self._callback_queue = callback_queue.method.queue
         channel.basic_qos(prefetch_count=1)
         channel.basic_consume(
             queue=self._callback_queue,
-            on_message_callback=self._callback,
+            on_message_callback=self._on_worker_message,
         )
         consumer_thread = threading.Thread(target=channel.start_consuming)
         consumer_thread.start()
 
-    def _callback(self, ch, method, properties, body):
+    def _on_worker_message(self, channel, method, properties, body):
         try:
             image_id = int(body.decode())
-            self._processed_images.append(image_id)
-            ch.basic_ack(delivery_tag=method.delivery_tag)
+            with self._lock:
+                self._processed_images.append(image_id)
+                self._pending_images.pop(image_id, None)
+
+            channel.basic_ack(delivery_tag=method.delivery_tag)
             logger.info(f"Server recieved {image_id}.")
         except Exception as e:
             logger.info(f"Server failed to recieve {image_id} due to {e}.")
 
     def store_image(self, image: str) -> int:
         image_id = self._counter
-        self._counter += 1
-        while True:
-            try:
-                self._task_channel.basic_publish(
-                    exchange="",
-                    routing_key="task_queue",
-                    properties=pika.BasicProperties(
-                        delivery_mode=2,
-                        reply_to=self._callback_queue,
-                    ),
-                    body=f"{image} {image_id}",
-                )
-                logger.info(f"Added {image_id} to task queue.")
-                return image_id
-            except Exception as e:
-                logger.info(f"Retransmit {image_id} due to {e}.")
-                time.sleep(1)
+        with self._lock:
+            self._pending_images[image_id] = image
+            self._counter += 1
+
+        logger.info(f"Added {image_id} to task queue.")
+        return image_id
 
     def get_processed_images(self) -> List[int]:
         try:
-            return self._processed_images
+            with self._lock:
+                return self._processed_images
         except Exception as e:
             logger.info(f"Failed to get list of processed images due to {e}.")
 
@@ -90,20 +118,20 @@ def create_app() -> Flask:
     """
     app = Flask(__name__)
 
-    server = Server(host=os.getenv("RABBITMQ_HOST"), port=os.getenv("RABBITMQ_PORT"))
+    server = Server(host=RABBITMQ_HOST, port=RABBITMQ_PORT)
 
-    @app.route(os.getenv("IMAGES_ENDPOINT"), methods=["POST"])
+    @app.route(IMAGES_ENDPOINT, methods=["POST"])
     def add_image():
         body = request.get_json(force=True)
         image_id = server.store_image(body["image_url"])
         return {"image_id": image_id}
 
-    @app.route(os.getenv("IMAGES_ENDPOINT"), methods=["GET"])
+    @app.route(IMAGES_ENDPOINT, methods=["GET"])
     def get_image_ids():
         image_ids = server.get_processed_images()
         return {"image_ids": image_ids}
 
-    @app.route(f"{os.getenv('IMAGES_ENDPOINT')}/<string:image_id>", methods=["GET"])
+    @app.route(f"{IMAGES_ENDPOINT}/<string:image_id>", methods=["GET"])
     def get_processing_result(image_id):
         result = server.get_image_description(image_id)
         if result is None:
@@ -118,4 +146,4 @@ app = create_app()
 
 if __name__ == "__main__":
     logging.basicConfig()
-    app.run(host=os.getenv("WEB_HOST"), port=os.getenv("WEB_PORT"))
+    app.run(host=WEB_HOST, port=WEB_PORT)
--- homework/05-practice-messaging/worker/worker.py
+++ homework/05-practice-messaging/worker/worker.py
@@ -7,11 +7,16 @@ from loguru import logger
 from caption import get_image_caption
 
 
+RABBITMQ_HOST = os.getenv("RABBITMQ_HOST")
+RABBITMQ_PORT = os.getenv("RABBITMQ_PORT")
+DATA_DIR = os.getenv("DATA_DIR")
+
+
 def callback(channel, method, properties, body):
     try:
         image, image_id = body.decode().split()
         logger.info(f"Worker received {image_id}.")
-        with open(f"/data/{image_id}.txt", "w") as file:
+        with open(f"{DATA_DIR}/{image_id}.txt", "w") as file:
             file.write(get_image_caption(image))
 
         channel.basic_publish(
@@ -27,15 +32,12 @@ def callback(channel, method, properties, body):
 
 if __name__ == "__main__":
     connection = pika.BlockingConnection(
-        pika.ConnectionParameters(
-            host=os.getenv("RABBITMQ_HOST"), port=os.getenv("RABBITMQ_PORT")
-        )
+        pika.ConnectionParameters(host=RABBITMQ_HOST, port=RABBITMQ_PORT)
     )
     channel = connection.channel()
-
-    channel.queue_declare(queue="task_queue", durable=True)
-    logger.info("Worker waiting for messages.")
-
+    channel.queue_declare(queue="task_queue")
     channel.basic_qos(prefetch_count=1)
     channel.basic_consume(queue="task_queue", on_message_callback=callback)
+
+    logger.info("Worker waiting for messages.")
     channel.start_consuming()
